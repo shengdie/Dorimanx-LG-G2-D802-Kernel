d56be7480c424f6fe6ed8c0a6bdf24a223a66db4
nikhil18 <nikhilgolechha1@gmail.com>
2014-09-17 07:30:50 -0700

    some fixes

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -437,6 +437,9 @@ extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
 extern struct cpufreq_governor cpufreq_gov_lazy;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lazy)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartmax)
 #endif
 
 
